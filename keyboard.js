cd2e98c1fd9e5af99c100c3aa7db0cd25d926285
kathyzxu <kx2204@columbia.edu>
2026-02-01 19:00:17 -0500

    Update keyboard.js

--- keyboard.js
+++ keyboard.js
@@ -27,79 +27,148 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     }
 
-    // map notes to keys on computer keyboard
     const keyboardFrequencyMap = {
-        '83': 261.63, // S - C
-        '68': 293.66, // D - D
-        '70': 329.63, // F - E
-        '71': 349.23, // G - F
-        '72': 391.99, // H - G
-        '74': 440.00, // J - A   
-        '75': 493.88,  // K - B 
-        '76': 523.26,  // L - C
-        '69': 277.18, // E - C#
-        '82': 311.13, // R - D#
-        '89': 369.99, // Y - F#
-        '85': 415.30, // U - G#
-        '73': 466.16, // I - A#
+        'Z': 261.63,  // C
+        'S': 277.18,  // C#
+        'X': 293.66,  // D
+        'D': 311.13,  // D#
+        'C': 329.63,  // E
+        'V': 349.23,  // F
+        'G': 369.99,  // F#
+        'B': 391.99,  // G
+        'H': 415.30,  // G#
+        'N': 440.00,  // A
+        'J': 466.16,  // A#
+        'M': 493.88,  // B
+
+        'Q': 523.25,  // C
+        'A': 554.37,  // C#
+        'W': 587.33,  // D
+        'F': 622.25,  // D#
+        'E': 659.26,  // E
+        'R': 698.46,  // F
+        'K': 739.99,  // F#
+        'T': 783.99,  // G
+        'L': 830.61,  // G#
+        'Y': 880.00,  // A
+        'O': 932.33,  // A#
+        'U': 987.77,  // B
+        'I': 1046.50, // C
     };
 
-    // set black and white keys to mimick piano
-    const blackKeys = ['69','82','89','85','73'];   
+    const keyColors = {
+        'Z': '#FF7F50',  // C
+        'S': '#ff5c77',  // C#
+        'X': '#ffb056',  // D
+        'D': '#ffea4c',  // D#
+        'C': '#b0e0e6',  // E
+        'V': '#5f9ea0',  // F
+        'G': '#63a9ff',  // F#
+        'B': '#f08080',  // G
+        'H': '#b44aff',  // G#
+        'N': '#ff4500',  // A
+        'J': '#8F00FF',  // A#
+        'M': '#FF1493',  // B
 
+        'Q': '#FFD700',  // C
+        'A': '#ffa07a',  // C#
+        'W': '#f0e68c',  // D
+        'F': '#20b2aa',  // D#
+        'E': '#ff69b4',  // E
+        'R': '#d3ff4f',  // F
+        'K': '#32cd32',  // F#
+        'T': '#ff6347',  // G
+        'L': '#9370db',  // G#
+        'Y': '#be27ff',  // A
+        'O': '#ff00ff',  // A#
+        'U': '#FF69B4',  // B
+        'I': '#ff6987', //C
+    };
+
+    // set key orders
+    const whiteKeyOrder = [
+        'Z', // C
+        'X', // D
+        'C', // E
+        'V', // F
+        'B', // G
+        'N', // A
+        'M', // B
+
+        'Q', // C
+        'W', // D
+        'E', // E
+        'R', // F
+        'T', // G
+        'Y', // A
+        'U', // B
+        'I', // C
+
+    ];
+
+    const blackKeys = [
+        'S', // C#
+        'D', // D#
+        'G', // F#
+        'H', // G#
+        'J', // A#
+
+        'A', // C# (upper)
+        'F', // D#
+        'K', // F#
+        'L', // G#
+        'O'  // A#
+    ];
+
+    const blackKeyOrder = blackKeys;
+
+    // containers
     const whiteContainer = document.getElementById('white-keys');
     const blackContainer = document.getElementById('black-keys');
-
     const buttons = {};
 
-    const whiteKeyOrder = ['83','68','70','71','72','74','75','76'];
-    const blackKeyOrder = ['69','82','89','85','73'];
+    // posiiton black keys between the correct white keys
+    const blackOffsets = {
+        'S': -378,   // C#
+        'D': -318,  // D#
 
-    for (const code of whiteKeyOrder) {
+        'G': -200,  // F#
+        'H': -140,  // G#
+        'J': -80,  // A#
+
+        'A': 37,  // C# upper
+        'F': 97,  // D# upper
+
+        'K': 213,  // F# upper
+        'L': 273,  // G# upper
+        'O': 333   // A# upper
+    };
+
+    // create white key buttons
+    for (const key of whiteKeyOrder) {
         const btn = document.createElement('button');
-        btn.textContent = keyCodeToChar(code); 
-        btn.dataset.key = code;
+        btn.textContent = key;
+        btn.dataset.key = key;
         btn.classList.add('white-key');
         whiteContainer.appendChild(btn);
-        buttons[code] = btn;
+        buttons[key] = btn;
     }
 
-    // position black keys between the correct white keys
-    const blackOffsets = {'69': -247,  '82': -175, '89': -25, '85': 50, '73': 122};    
-
-    for (const code of blackKeyOrder) {
+    // create black key buttons
+    for (const key of blackKeyOrder) {
         const btn = document.createElement('button');
-        btn.textContent = keyCodeToChar(code);
-        btn.dataset.key = code;
+        btn.textContent = key;
+        btn.dataset.key = key;
         btn.classList.add('black-key');
         btn.style.color = 'white';
+        btn.style.left = (blackOffsets[key]-30) + 'px';
         blackContainer.appendChild(btn);
-
-        btn.style.left = blackOffsets[code] + 'px';
-
-        buttons[code] = btn;
+        buttons[key] = btn;
     }
 
     function keyCodeToChar(code) {
         return String.fromCharCode(code);
     }
-
-    // set colors when keys are pressed
-    const keyColors = {
-        '83': '#ff5c77', // S - C
-        '69': '#ffb056', // E - C#
-        '68': '#ffea4c', // D - D
-        '82': '#d3ff4f', // R - D#
-        '70': '#5cff5c', // F - E
-        '71': '#63a9ff', // G - F
-        '89': '#be27ff', // Y - F#
-        '72': '#4B0082', // H - G
-        '85': '#FF00FF', // U - G#
-        '74': '#8F00FF', // J - A
-        '73': '#FF69B4', // I - A#
-        '75': '#FF1493', // K - B
-        '76': '#FF69B4', // L - C
-    };
 
 
     // create key press & release events
@@ -110,7 +179,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // play corresponding note when user presses a valid key 
     function keyDown(event) {
-        const key = event.keyCode.toString();
+        const key = event.key.toUpperCase();
         if (keyboardFrequencyMap[key] && !activeOscillators[key]) {
             playNote(key);
 
@@ -123,7 +192,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // releases corresponding notes when user stops pressing a key
     function keyUp(event) {
-        const key = event.keyCode.toString();
+        const key = event.key.toUpperCase();
         if (activeOscillators[key]) {
             // get current oscillator
             const {osc, gainNode, release} = activeOscillators[key]; 
@@ -188,17 +257,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // art banner on top of webpage
     const asciiStrip = document.getElementById('ascii-strip');
     const asciiArt = `
-
-            ⢠⡾⠲⠶⣤⣀⣠⣤⣤⣤⡿⠛⠿⡴⠾⠛⢻⡆⠀⠀⠀
-        ⠀⠀⠀⣼⠁⠀⠀⠀⠉⠁⠀⢀⣿⠐⡿⣿⠿⣶⣤⣤⣷⡀⠀⠀
-        ⠀⠀⠀⢹⡶⠀⠀⠀⠀⠀⠀⠈⢯⣡⣿⣿⣀⣸⣿⣦⢓⡟⠀⠀
-        ⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠹⣍⣭⣾⢱⡀⠀
-        ⠀⣀⣸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⣆
-        ⠈⠉⢹⣉⡁⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⢀⣉⣿⣈⠁
-        ⠀⠐⠋⢯⣁⣄⢀⣀⣀⡀⠀⠯⠽⠀⢀⣀⣀⡀⠀⣤⡟⠀⠉⠀
-        ⠀⠀⠴⠛⠙⣷⠋⠉⠉⠙⣆⠀⠀⢰⡟⠉⠈⠙⢷⠟⠉⠙⠂⠀
-        ⠀⠀⠀⠀⠀⠘⣆⣠⣤⣴⠟⠛⠛⠛⢧⣤⣤⣀⡾⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡇⠀⠀⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣼⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⣡⣇⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢠⣿⣿⠟⢻⣿⠤⠖⠒⠚⠉⠉⠉⠉⠉⠉⢩⡟⣹⠋⣿⠉⠉⠛⠒⣺⡤⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⡟⠀⢠⣿⡇⠀⢀⡄⠀⠀⠀⠀⠀⠀⣏⣼⣃⣠⣽⡤⠤⢴⠯⣭⠧⢼⣎⡳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⣿⡇⠀⣼⣿⡇⠀⠰⣇⣠⡤⠴⠒⠚⠉⣿⠁⣤⣾⣿⡇⢀⣈⣉⣥⡤⢼⠬⣯⣛⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⢻⣧⣼⣿⣿⠧⠒⠋⣏⣄⠀⠀⠀⠀⠀⢹⣀⡿⠿⠛⠉⠉⠁⠀⣀⣴⣾⠾⠓⠲⡯⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣞⣇⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣿⣿⣿⡟⠀⠀⠀⣇⡿⠃⣀⡤⠴⠚⢹⡇⠀⠀⣀⡠⠖⠚⠉⢀⣀⠤⠖⠚⣹⣋⣏⡟⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢯⠏⢿⠀⠀⠀
+⠀⠀⣴⣿⣿⣿⡟⠀⠀⠀⢀⡫⠖⠋⠁⠀⠀⣠⣤⣧⠴⠋⠁⠀⣀⡤⠚⠉⠀⠀⠀⣰⠃⡽⠣⣏⡧⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⢸⣧⣴⣾⣀⠀
+⢀⣾⣿⣿⡿⢻⣷⢀⡤⠞⠉⠀⠀⠀⠀⢀⣼⣿⡿⠏⠀⣀⡴⠛⠁⠀⠀⠀⠀⢀⡴⠁⡴⠁⡼⢸⣷⢸⠀⠀⠀⠀⠈⣧⠀⠀⠀⣀⡠⠔⠚⠉⠁⠀⠀⠀⠈⡇⠉⠁⠀
+⣼⣿⣿⠟⠀⣨⣿⣿⣿⣷⣦⡀⢀⣠⠖⠋⠀⠀⢀⣤⠞⠁⠀⠀⠀⠀⠀⣦⣠⠞⢶⡞⠣⣼⢁⡼⢁⡏⠀⠀⠀⠀⠀⢘⣧⠔⠋⠁⢀⡀⣀⡠⠴⠒⠚⢩⣽⡯⠉⠉⠉
+⣿⣿⡏⠀⣾⣿⣿⣿⡿⢿⣿⣿⣏⠁⠀⠀⢠⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⢰⠛⢦⠎⢉⠿⣌⡞⢀⡞⠀⠀⠀⢀⡤⠚⠉⠘⣇⣀⠤⠚⣯⠁⠀⠀⣀⣤⠼⠟⠓⠒⠒⠒⠒
+⣿⣿⡇⠰⣿⣿⠁⢻⣧⠀⠹⣿⣿⠀⣠⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⢠⢯⣷⡏⢰⢿⢇⠞⠀⣀⠴⠚⠁⠀⢀⣠⡶⢻⡇⠀⢀⡸⡶⠚⠉⠀⢸⡓⣦⣠⠤⠤⠒
+⠘⣿⣇⠀⢻⣿⣄⠈⣿⡆⠀⣿⣿⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⣮⣸⣋⣰⣋⣤⠾⠒⠉⠀⣠⣆⡠⠖⠋⠹⠤⣞⠴⠚⠁⠀⢻⣀⡤⠖⠊⡏⢸⠇⠀⣀⠤
+⠀⠘⢿⣷⣄⡈⠛⠛⢸⣿⣾⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢾⣠⠇⢠⠋⠀⣀⣠⣾⡟⡏⠀⠀⣀⠴⠊⠁⠀⢀⡠⣶⣿⡇⠀⢀⣴⣾⠼⠚⠉⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠈⠙⠛⠿⠿⠟⢻⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢾⢉⠏⠉⠉⠁⣻⢋⣤⡧⠴⠋⢷⡀⢀⡤⠚⠉⠀⠿⠿⣃⠴⠚⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣀⡀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣽⠒⠠⠤⠔⣿⠛⠛⠁⠀⣀⡬⢿⠁⠀⠀⠀⣀⡴⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣰⣿⣿⣿⣆⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠦⣀⣀⣤⣿⣀⡠⠴⠊⠁⠀⢈⣧⣀⠴⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢻⣿⣿⣿⠟⢀⣼⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠈⠉⠁⠀⠀⠀⠀⢀⣠⡿⣻⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠛⠿⠿⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠲⠦⠶⠒⠚⠉⠁⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀
     `;
+
+        //     ⢠⡾⠲⠶⣤⣀⣠⣤⣤⣤⡿⠛⠿⡴⠾⠛⢻⡆⠀⠀⠀
+        // ⠀⠀⠀⣼⠁⠀⠀⠀⠉⠁⠀⢀⣿⠐⡿⣿⠿⣶⣤⣤⣷⡀⠀⠀
+        // ⠀⠀⠀⢹⡶⠀⠀⠀⠀⠀⠀⠈⢯⣡⣿⣿⣀⣸⣿⣦⢓⡟⠀⠀
+        // ⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠹⣍⣭⣾⢱⡀⠀
+        // ⠀⣀⣸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⣆
+        // ⠈⠉⢹⣉⡁⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⢀⣉⣿⣈⠁
+        // ⠀⠐⠋⢯⣁⣄⢀⣀⣀⡀⠀⠯⠽⠀⢀⣀⣀⡀⠀⣤⡟⠀⠉⠀
+        // ⠀⠀⠴⠛⠙⣷⠋⠉⠉⠙⣆⠀⠀⢰⡟⠉⠈⠙⢷⠟⠉⠙⠂⠀
+        // ⠀⠀⠀⠀⠀⠘⣆⣠⣤⣴⠟⠛⠛⠛⢧⣤⣤⣀⡾
 
     function createAsciiCopy() {
     const span = document.createElement('span');
